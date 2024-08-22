f62ef36bc58810ffdee4a6a8bd64713f86e3dd0f
hariprasath36 <hariprasa36@gmail.com>
2024-08-22 11:14:37 +0530

    Finished

---
+++ src/main/java/admin_user/AdminAndUserRegistrationApplication.java
@@ -0,0 +1,13 @@
+package admin_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminAndUserRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminAndUserRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/main/java/admin_user/service/CustomUserDetail.java
@@ -0,0 +1,42 @@
+package admin_user.service;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import admin_user.model.User;
+
+public class CustomUserDetail implements UserDetails{
+
+	private User user;
+	public CustomUserDetail(User user) {
+		
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return List.of(() -> user.getRole());
+	}
+	
+	public String getFullname() {
+		return user.getFullname();
+	}
+
+	
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return user.getEmail();
+	}
+
+}
